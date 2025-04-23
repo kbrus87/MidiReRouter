b92915d32437a10997ebcec2e2713ad6cb2660f1
bjr <bjramariutn@hotmail.com>
2025-04-23 10:37:40 -0300

    link to main page"

--- GUI/midirouter-gui/src/components/Header/Header.tsx
+++ GUI/midirouter-gui/src/components/Header/Header.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Title from "./Title";
 
 function Header() {
+    const handleOnClick = () => {
+        window.open("https://kbrus87.github.io/#/midiRouter/", "_blank")
+    }
     return <div className="header">
-        <div className="name-block">
+        <div onClick={handleOnClick} className="name-block">
             <Title text="Midi Router" className="name align-left"></Title>
         </div>
-        <div className="logo-block">
+        <div onClick={handleOnClick} className="logo-block">
             <Title text="by" span="BJR" spanclassName="bjr" className="logo-upside align-right"></Title>
             <Title text="garbageAudio.com" className="logo-downside align-right"></Title>
         </div>
